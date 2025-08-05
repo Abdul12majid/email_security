4f807a6dd17223eadefa94de41217eb4d5fc94e2
Abdul12majid <yisaabdulmajid@gmail.com>
2025-08-05 06:51:06 +0100

    user auth setup

--- users/views.py
+++ users/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import LoginSerializer, SignUpSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
+@api_view(['POST'])
+def login_user(request):
+	serializer = LoginSerializer(data=request.data)
+	if serializer.is_valid():
+		username = request.data['username']
+		password = request.data['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return Response({'Info': 'Login Successful'})
+		else:
+			return Response({'Info': 'Incorrect username or password'})
+
+	return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST', 'GET'])	
+def register(request):
+	serializer = SignUpSerializer(data=request.data)
+	if serializer.is_valid():
+		serializer.save()
+		response = {
+			"info":"Registeration successful",
+			"user info":serializer.data
+		}
+		return Response(data=response, status=status.HTTP_201_CREATED)
+	return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST', 'GET'])	
+def logout_user(request):
+	logout(request)
+	return Response({'Info': "You've been logged out"})
+
